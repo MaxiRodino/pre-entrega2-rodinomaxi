f9eab27dde421373619f1f17ee9bbea725680571
Maxi Rodino <maximilianorodino@gmail.com>
2023-05-17 22:48:17 -0300

    pre-entrega

--- js/main.js
+++ js/main.js
@@ -1,57 +1,81 @@
 let nombreIngresado = prompt ("Ingrese su nombre");
-
-while (nombreIngresado.toUpperCase()){
-    nombreIngresado = prompt ("Hola" +" " + nombreIngresado + " " + "bienvenid@ al taller");
-}
-let servicios = propmt ("Ingrese un servicio deseado:\n \n Lavado de vehículo\n Cambio de aceite y filtro\n Pulido detailing\n Mecánica ligera\n Limpieza de interiores\n Revisión y reparación de frenos").toUpperCase();
-
-while((servicios != "Lavado de vehiculo") && (servicios != "Cambio de aceite y filtro") && (servicios != "Pulido detailing") && (servicios != "Mecánica ligera") && (servicios != "Limpieza de interiores") && (servicios != "Revisión y reparación de frenos")){
-    servicios = prompt ("Ingrese un servicio deseado:\n \n Lavado de vehículo\n Cambio de aceite y filtro\n Pulido detailing\n Mecánica ligera\n Limpieza de interiores\n Revisión y reparación de frenos").toUpperCase();
-}
-
 console.log(nombreIngresado);
-console.log(servicios);
+
+alert("Hola" +" " + nombreIngresado + " " + "bienvenid@ al taller")
+
+let servicios = prompt ("Ingrese un servicio deseado:\n"+
+                        "1 - Lavado de vehículo\n"+
+                        "2 - Cambio de aceite y filtro\n"+
+                        "3 - Pulido detailing\n"+
+                        "4 - Mecánica ligera\n"+
+                        "5 - Limpieza de interiores\n"+
+                        "6 - Revisión y reparación de frenos");
+
+while(
+  (servicios !== "1")&&
+  (servicios !== "2")&&
+  (servicios !== "3")&&
+  (servicios !== "4")&&
+  (servicios !== "5")&&
+  (servicios !== "6")){
+ 
+  alert("por favor ingresa un servicio");
+  servicios = prompt ("Ingrese un servicio deseado:\n"+
+                        "1 - Lavado de vehículo\n"+
+                        "2 - Cambio de aceite y filtro\n"+
+                        "3 - Pulido detailing\n"+
+                        "4 - Mecánica ligera\n"+
+                        "5 - Limpieza de interiores\n"+
+                        "6 - Revisión y reparación de frenos");
+ 
+ 
+}
+
 
 const calcularValorServicios = (servicios) =>{
-switch(servicios){
-    case "Lavado de vehiculo":
-        ValorServicios = 1500
+  let valorServicios = 0;
+  switch(servicios){
+    case "1":
+        valorServicios = 1500
         alert("Elegiste lavado de vehiculo");
         break;
-    case "Cambio de aceite y filtro":
-        ValorServicios = 15000
+    case "2":
+        valorServicios = 15000
         alert("Elegiste cambio de aceite y filtro");
         break;
-    case "Pulido detailing":
-        ValorServicios = 20000
-        alert("Elegiste pulido detailing");
+    case "3":
+        valorServicios = 20000
+        alert("");
         break;
-    case "Mecánica ligera":
-        ValorServicios = 10000
+    case "4":
+        valorServicios = 10000
         alert("Elegiste mecánica ligera");
         break;
-    case "Limpieza de interiores":
-        ValorServicios = 8000
+    case "5":
+        valorServicios = 8000
         alert("Elegiste limpieza de interiores");
         break;
-    case "Revisión y reparacion de frenos":
-        ValorServicios = 15000
+    case "6":
+        valorServicios = 15000
         alert("Elegiste revisión y reparacion de frenos");
         break;
     default:
-        ValorServicios = 00000
+        valorServicios = 00000
         alert("Ingrese una opción");
         break;
 }
-    return ValorServicios;
+    return valorServicios;
 }
 
-const preciofinal = (ValorServicios) => {
-    return servicios + servicios
-}
-let ValorServicios = calcularValorServicios (servicios);
+let valorDelServicio = calcularValorServicios (servicios);
 
-console.log (ValorServicios);
-console.log ("valor del servicio a:" + servicios + "\n" + "valor del servicio b:" + servicios + "\n" + preciofinal(ValorServicios));
-let salida = "El presupuesto estimado es:" + servicios + "\n" + servicios +  preciofinal(ValorServicios);
-alert (salida);
+let pregunta = prompt("paga en efectivo?").toUpperCase()
+
+let descuento = 0;
+
+if(pregunta === "SI"){
+  alert("elegiste efectivo, tenes un descuento del 30%")
+  descuento = valorDelServicio * 30 /100;
+}
+
+alert(valorDelServicio - descuento);
